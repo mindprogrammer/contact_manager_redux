4f18fe0d672a072a325e0fb9fa38dd968684fa9d
Khoi Nguyen <khoi.ntq@gmail.com>
2018-09-16 14:23:07 -0400

    Ininital commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div>
+      <h1 className="display-4">About Contact Manager</h1>
+      <p className="lead">
+        Simple app to manage contacts built by React and Redux
+      </p>
+      <p className="text-secondary">Version 1.0.0</p>
+      <p className="text-secondary">Author: Khoi Nguyen</p>
+    </div>
+  );
+};
